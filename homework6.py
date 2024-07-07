31f1321d9a40d163931ecd523901898b2c45088a
Vladislav <vladislav.andreev.2017@mail.ru>
2024-07-07 16:44:31 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,14 @@
+my_dict = {'Denis': 1999, 'Ivan': 2003, 'Vlad': 2000}
+print('Dict:', my_dict)
+print('Existing value:',my_dict.get('Ivan'))
+print('Not existing value:', my_dict.get('Petr'))
+my_dict.update({'Alex':1980,'Max':2001})
+print('Modified dictionary:',my_dict)
+del my_dict['Ivan']
+print('Deleted value:', 'Ivan')
+
+my_set = {1, 1, 2, 4, 4, 99, 99,'Ivan'}
+print('Set:', my_set)
+my_set.update({'Felix', 3, 77})
+my_set.discard(4)
+print('Modified set:', my_set)
